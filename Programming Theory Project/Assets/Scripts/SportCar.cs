86b94f2c42b74ef9e237a9efbbc3f14910a7eb5e
rodrigo-vargas <rodrigovargas123@gmail.com>
2022-01-29 23:21:53 -0300

    Added main menu

---
+++ Programming Theory Project/Assets/Scripts/SportCar.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SportCar : Car
+{
+    public SportCar()
+    {
+        HorsePower = 500;
+    }
+}
